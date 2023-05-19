6b67740c21b95a96d9740c9108e2eee6386f8794
Dinara <khdinova@gmail.com>
2023-05-19 18:46:42 +0300

    мяу

---
+++ app.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+from dash import Dash, html, dcc
+import plotly.express as px
+
+app = Dash(__name__)
+
+colors = {
+    'background': '#111111',
+    'text': '#7FDBFF'
+}
+
